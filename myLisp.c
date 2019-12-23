58fc528bdc20984e24a4dbe6a223cea7a6eb6081
Lucas Lin <linlucas33@gmail.com>
2019-12-23 13:30:24 -0800

    Added parsing functions

--- myLisp.c
+++ myLisp.c
@@ -10,14 +10,36 @@ int main(int argc, char** argv) {
     mpc_parser_t* Expr = mpc_new("expr");
     mpc_parser_t* MyLisp = mpc_new("myLisp");
 
-    puts("myLisp version 0.0.3");
+    // define parsers
+    mpca_lang(MPCA_LANG_DEFAULT, 
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      myLisp    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, MyLisp);
+
+    puts("myLisp version 0.0.4");
     puts("Press ctrl + c to exit\n");
 
     while (1) {
         char* input = readline("myLisp > ");
         add_history(input);
-        printf("%s\n", input);
+        
+        // parse user input
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, MyLisp, &r)) {
+            // on success print and delete the AST
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            // on failure print and delete the error
+            mpc_ast_print(r.error);
+            mpc_ast_delete(r.error);
+        }
         free(input);
     }
+    mpc_cleanup(4, Number, Operator, Expr, MyLisp);
     return 0;
 }
