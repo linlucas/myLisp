c3b7fd6ac47fee00d2eacc79bd7a8cf4b82942db
Lucas Lin <linlucas33@gmail.com>
2019-12-23 13:14:32 -0800

    Created parsers

--- myLisp.c
+++ myLisp.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <editline/readline.h>
+#include "mpc.h"
 
 int main(int argc, char** argv) {
+    // create parsers
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* MyLisp = mpc_new("myLisp");
+
     puts("myLisp version 0.0.3");
     puts("Press ctrl + c to exit\n");
 
