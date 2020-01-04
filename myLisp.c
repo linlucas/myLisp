a9673cf160501211aa8cbe9fc48cc42c7f853492
Lucas Lin <linlucas33@gmail.com>
2020-01-03 16:32:14 -0800

    Added math functions

--- myLisp.c
+++ myLisp.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <editline/readline.h>
 #include "mpc.h"
+
+long getOperationType(long x, char *op, long y);
+long evaluate(mpc_ast_t *t);
+
 
 int main(int argc, char *argv[]) {
     // create parsers
@@ -30,8 +35,8 @@ int main(int argc, char *argv[]) {
         // parse user input
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, MyLisp, &r)) {
-            // on success print and delete the AST
-            mpc_ast_print(r.output);
+            long result = evaluate(r.output);
+            printf("li\n", result);
             mpc_ast_delete(r.output);
         } else {
             // on failure print and delete the error
@@ -42,4 +47,34 @@ int main(int argc, char *argv[]) {
     }
     mpc_cleanup(4, Number, Operator, Expr, MyLisp);
     return 0;
+}
+
+long getOperationType(long x, char *op, long y) {
+    if (strcmp(op, "+") == 0) {
+        return x + y;
+    } else if (strcmp(op, "-") == 0) {
+        return x - y;
+    } else if (strcmp(op, "*") == 0) {
+        return x * y;
+    } else if (strcmp(op, "/") == 0) {
+        return x / y;
+    }
+    return 0;
+}
+
+long evaluate(mpc_ast_t *t) {
+    char *end;
+    int i = 3;
+
+    if (strstr(t->tag, "number")) {
+        return strtol(t->contents, &end, 10);
+    }
+    char *op = t->children[1]->contents;
+    long x = evaluate(t->children[2]);
+
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = getOperationType(x, op, evaluate(t->children[i]));
+        ++i;
+    }
+    return x;
 }
